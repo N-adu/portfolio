7e7de21984a0cae112d9f4c8c18c8e0e7caae732
N Adu <64503636+N-adu@users.noreply.github.com>
2021-06-29 17:43:47 +0100

    Update script.js
    Edited the scroll feature so that if the user scrolls back up to the top, the previous setting applies.

--- script.js
+++ script.js
@@ -4,5 +4,7 @@ const header = document.querySelector('header');
 window.addEventListener('scroll', () => {
     if(scrollY > 0){
         header.classList.add('sticky')
+    }else if(scrollY == 0){
+        header.classList.remove('sticky')
     }
 });
