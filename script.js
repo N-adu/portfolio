77de44b8102facaa90f4a421256bb13d2a774fbb
N Adu (Tashelle Dev) <64503636+N-adu@users.noreply.github.com>
2021-10-22 23:37:57 +0100

    Add files via upload
    Replacement js file.

---
+++ script.js
@@ -0,0 +1,18 @@
+// sticky Header
+const header = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+    if(scrollY > 0){
+        header.classList.add('sticky')
+    }else if(scrollY == 0){
+        header.classList.remove('sticky')
+    }
+});
+
+
+const homeText = document.querySelector('.home-txt');
+
+window.onload = function animationLoad () {
+    homeText.classList.add('show');
+}
+
